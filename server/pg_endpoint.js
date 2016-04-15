1833a8f13c780b476bef24f58a15e22f25e1bdfd
Dario Seminara <dseminara@tbx.net.com>
2016-04-15 12:27:11 -0300

    improved error handling on pg_endpoints

--- server/pg_endpoint.js
+++ server/pg_endpoint.js
@@ -8,66 +8,81 @@ module.exports = function(tableName, queryList, queryCount, queryGet, listWrappe
 	var app = express();
 	options = options ||{};
 
-	app.delete("/:id", function(req, res) {
-		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-			var query = q.denodeify(client.query.bind(client));
-			query("DELETE FROM " + tableName + " WHERE id = $1::int", [req.params.id])
-			.finally(done)
-			.then(function() {
-				res.sendStatus(204);
+	var pgConnect = q.denodeify(function(url, callback) {
+		pg.connect(url, function(err, client, done) {
+			callback(err, {
+				client: client,
+				done: done
 			});
 		});
 	});
 
+	app.delete("/:id", function(req, res) {
+		pgConnect(process.env.DATABASE_URL).then(function(connection) {
+			var client = connection.client;
+			var query = q.denodeify(client.query.bind(client));
+			return query("DELETE FROM " + tableName + " WHERE id = $1::int", [req.params.id])
+				.finally(connection.done);
+		})
+			.then(function() {
+				res.sendStatus(204);
+			}).catch(function(err) {
+				console.error(err);
+				res.status(500).send(err.toString());
+			});
+	});
+
 	if (options.fields) {
 		app.post("/", function(req, res) {
-			var currentFields = options.fields.filter(function(fieldName) {
-				return typeof req.body[fieldName] !== "undefined";
-			});
+			pgConnect(process.env.DATABASE_URL).then(function(connection) {
+				var currentFields = options.fields.filter(function(fieldName) {
+					return typeof req.body[fieldName] !== "undefined";
+				});
 
-			var queryText = "INSERT INTO " + tableName + " (" + currentFields.join(",") + ") VALUES (" + currentFields.map(function(fieldName, index) {
-				return "$"+(index+1);
-			}).join(",") + ")";
+				var queryText = "INSERT INTO " + tableName + " (" + currentFields.join(",") + ") VALUES (" + currentFields.map(function(fieldName, index) {
+					return "$"+(index+1);
+				}).join(",") + ")";
 
-			pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+				var client = connection.client;
 				var query = q.denodeify(client.query.bind(client));
-				query(queryText, currentFields.map(function(fieldName){
+				return query(queryText, currentFields.map(function(fieldName){
 					return req.body[fieldName];
 				}))
-				.finally(done)
+					.finally(connection.done);
+			})
 				.then(function() {
 					res.sendStatus(204);
 				}).catch(function(err) {
 					console.error(err);
 					res.status(500).send(err.toString());
 				});
-			});
 		});	
 
 		app.put("/:id", function(req, res) {
-			var currentFields = options.fields.filter(function(fieldName) {
-				return typeof req.body[fieldName] !== "undefined";
-			});
+			pgConnect(process.env.DATABASE_URL).then(function(connection) {
+				var currentFields = options.fields.filter(function(fieldName) {
+					return typeof req.body[fieldName] !== "undefined";
+				});
 
-			var queryText = "UPDATE " + tableName + " SET " + currentFields.map(function(fieldName, index) {
-				return fieldName + "=$"+(index+2);
-			}).join(",") + " WHERE id = $1::int";
+				var queryText = "UPDATE " + tableName + " SET " + currentFields.map(function(fieldName, index) {
+					return fieldName + "=$"+(index+2);
+				}).join(",") + " WHERE id = $1::int";
 
-			pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+				var client = connection.client;
 				var query = q.denodeify(client.query.bind(client));
 				var queryParams = [req.params.id].concat(currentFields.map(function(fieldName){
 					return req.body[fieldName];
 				}));
 				
-				query(queryText, queryParams)
-				.finally(done)
+				return query(queryText, queryParams)
+					.finally(connection.done)
+			})
 				.then(function() {
 					res.sendStatus(204);
 				}).catch(function(err) {
 					console.error(err);
 					res.status(500).send(err.toString());
 				});
-			});
 		});	
 
 	}
@@ -80,17 +95,17 @@ module.exports = function(tableName, queryList, queryCount, queryGet, listWrappe
 			wrapperInstance = identity;
 		}
 
-		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		pgConnect(process.env.DATABASE_URL).then(function(connection) {
+			var client = connection.client;
 			var query = q.denodeify(client.query.bind(client));
-			query(queryGet, [req.params.id])
-			.finally(done)
-			.then(function(result) {
+			return query(queryGet, [req.params.id])
+				.finally(connection.done);
+		}).then(function(result) {
 				res.send(wrapperInstance(result.rows[0]));
 			}).catch(function(err) {
 				console.error(err);
 				res.status(500).send(err.toString());
 			});
-		});
 	});
 
 	app.get("/", function(req, res) {
@@ -104,30 +119,30 @@ module.exports = function(tableName, queryList, queryCount, queryGet, listWrappe
 			wrapperInstance = identity;
 		}
 
-		pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+		pgConnect(process.env.DATABASE_URL).then(function(connection) {
+			var client = connection.client;
 			var query = q.denodeify(client.query.bind(client));
 
 			var rows = queryList(query, req, offset, limit); // query(queryList, [offset, limit]);
 			var total = queryCount(query, req); //query("SELECT COUNT(*) FROM "+tableName);
 
-			q.all([rows, total])
-				.finally(done)
-				.spread(function(rows, total) {
-					res.send({
-						paging: {
-							limit: limit,
-							offset: offset,
-							total: total.rows[0].count
-						},
-						results: rows.rows.map(wrapperInstance)
-					});
-
-				})
-				.catch(function(err) {
-					console.error(err);
-					res.status(500).send(err.toString());
+			return q.all([rows, total])
+				.finally(connection.done);
+		}).spread(function(rows, total) {
+				res.send({
+					paging: {
+						limit: limit,
+						offset: offset,
+						total: total.rows[0].count
+					},
+					results: rows.rows.map(wrapperInstance)
 				});
-		});
+
+			})
+			.catch(function(err) {
+				console.error(err);
+				res.status(500).send(err.toString());
+			});
 	});
 
 	return app;
