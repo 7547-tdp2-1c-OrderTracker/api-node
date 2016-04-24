97969a879342cf9a64d0644f6e4c9c8cc7323ab7
Dario Seminara <darios3@gmail.com>
2016-04-24 10:47:30 -0300

    refactor: sequelize on clients

--- server/clients.js
+++ server/clients.js
@@ -1,38 +1,24 @@
-var express = require("express");
+var sequelize_endpoint = require("./sequelize_endpoint");
+var Sequelize = require("sequelize");
 
-var pg_endpoint = require("./pg_endpoint");
-var queryList = function(query, req, offset, limit) {
-	return query("SELECT * FROM clients ORDER BY lastname OFFSET $1::int LIMIT $2::int", [offset,limit]);
-};
-var queryCount = function(query, req) {
-	return query("SELECT COUNT(*) FROM clients");
-};
-var queryGet = "SELECT * FROM clients WHERE id = $1::int";
-
-var clientMap = function(req, res) {
-	return function(client) {
-		client.sellerType = client.seller_type;
-		delete client.seller_type;
-		return client;
-	};
-};
-
-var app = express();
-
-var clients = pg_endpoint("clients", queryList, queryCount, queryGet, clientMap, clientMap, {
-	fields: ["name", "lastname", "avatar", "thumbnail", "cuil", "address", "phone_number", "email", "lat", "lon", "seller_type"]
+var Client = sequelize_endpoint.sequelize.define('clients', {
+  name: Sequelize.STRING,
+  lastname: Sequelize.STRING,
+  avatar: Sequelize.STRING,
+  thumbnail: Sequelize.STRING,
+  cuil: Sequelize.STRING,
+  address: Sequelize.STRING,
+  phone_number: Sequelize.STRING,
+  email: Sequelize.STRING,
+  lat: Sequelize.REAL,
+  lon: Sequelize.REAL,
+  sellerType: {
+  	type: Sequelize.STRING,
+  	field: 'seller_type'
+  }
+}, {
+  freezeTableName: true,
+  timestamps: false
 });
 
-var readAliasFields = function(req, res, next) {
-	if (req.method === "POST" || req.method === "PUT") {
-		if (req.body.sellerType) {
-			req.body.seller_type = req.body.sellerType;
-		}
-	}
-	next();
-};
-
-app.use(readAliasFields);
-app.use(clients);
-
-module.exports = app;
+module.exports = sequelize_endpoint(Client);
