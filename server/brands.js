26e00a33028d57f812b0df3f6e5f16f0099698ad
Dario Seminara <darios3@gmail.com>
2016-04-23 22:48:34 -0300

    refactor: implemented sequelize for brands

--- server/brands.js
+++ server/brands.js
@@ -1,35 +1,13 @@
-var pg_endpoint = require("./pg_endpoint");
+var sequelize_endpoint = require("./sequelize_endpoint");
+var Sequelize = require("sequelize");
 
-var mapList = function(req, res) {
-	var fullUrl = req.protocol + '://' + req.get('host');
-	if (req.get('port')) {
-		fullUrl = fullUrl + ":" + req.get('port');
-	}
-
-	fullUrl = fullUrl + "/images";
-
-	return function(brand) {
-		if (brand.thumbnail && brand.thumbnail[0] === "/") {
-			brand.thumbnail = fullUrl + brand.thumbnail;
-		}
-		return {
-			id: brand.id,
-			name: brand.name,
-			picture: brand.picture
-		};
-	}
-};
-
-var mapGet = mapList;
-var queryList = function(query, req, offset, limit) {
-	return query("SELECT * FROM brands ORDER BY name OFFSET $1::int LIMIT $2::int", [offset, limit]);
-};
-var queryCount = function(query, req) {
-	return query("SELECT COUNT(*) FROM brands");
-};
-var queryGet = "SELECT * FROM brands WHERE brands.id = $1::int";
-
-module.exports = pg_endpoint("brands", queryList, queryCount, queryGet, mapList, mapGet, {
-	default_limit: 99999999,
-	fields: ["name", "picture"]
+var Brand = sequelize_endpoint.sequelize.define('brands', {
+  name: Sequelize.STRING,
+  picture: Sequelize.STRING,
+  code: Sequelize.STRING
+}, {
+  freezeTableName: true,
+  timestamps: false
 });
+
+module.exports = sequelize_endpoint(Brand);
---
+++ server/sequelize_endpoint.js
@@ -0,0 +1,100 @@
+var express = require("express");
+var Sequelize = require("sequelize")
+var q = require("q");
+
+var sequelize = new Sequelize(process.env.DATABASE_URL);
+var promised = function(f) {
+	return function(req, res) {
+		f(req, res)
+			.then(function(value) {
+				res.status(value.status).send(value.body);
+			})
+			.catch(function(err) {
+				res.status(500).send(err.toString());
+			});
+	};
+};
+
+var ret = function(model, options) {
+	var app = express();
+	var base;
+
+	options = options || {};
+	base = options.base||"";
+
+	// Create
+	app.post(base, promised(function(req, res) {
+		return model.create(req.body).then(function(instance) {
+			return {
+				body: instance.dataValues,
+				status: 200
+			};
+		});
+	}));	
+
+	// Read
+	app.get(base + "/:id", promised(function(req, res) {
+		return model.findOne({where: {id: req.params.id}}).then(function(instance) {
+			if (!instance) return {status: 404, body: "Not Found"};
+
+			return {
+				body: instance.dataValues,
+				status: 200
+			};
+		});
+	}));
+
+	// list
+	app.get(base, promised(function(req, res) {
+		var offset = parseInt(req.query.offset || '0');
+		var limit = parseInt(req.query.limit || options.default_limit || '20');
+
+		return q.all([
+			model.findAll({limit: limit, offset: offset}),
+			model.findOne({
+				attributes: [[sequelize.fn('COUNT', sequelize.col("*")), "count"]]
+			})
+		]).spread(function(instances, count) {
+
+			console.log(count.dataValues);
+			return {
+				body: {
+					paging: {
+						limit: limit,
+						offset: offset,
+						total: count.get("count")
+					},
+					results: instances
+				},
+				status: 200
+			};
+		});
+	}));
+
+	// Update
+	app.put(base + "/:id", promised(function(req, res) {
+		return model.update(req.body, {where: {id: req.params.id}}).then(function(instance) {
+			return model.findOne({where: {id: req.params.id}}).then(function(instance) {
+				return {
+					body: instance.dataValues,
+					status: 200
+				};
+			});
+		});
+	}));
+
+	// Destroy
+	app.del(base + "/:id", promised(function(req, res) {
+		return model.destroy({where: {id: req.params.id}}).then(function() {
+			return {
+				status: 204
+			};
+		});
+	}));
+
+	return app;
+};
+
+ret.sequelize = sequelize;
+
+module.exports = ret;
