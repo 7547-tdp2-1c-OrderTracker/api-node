80d1b1ba8c35c53185882fcfc1b9fb6e67e8451d
Dario Seminara <darios3@gmail.com>
2016-04-28 21:33:01 -0300

    implemented ST_Distance order for clients

--- server/clients.js
+++ server/clients.js
@@ -3,7 +3,17 @@ var sequelize_endpoint = require("./sequelize_endpoint");
 var Client = require("./models/client");
 
 module.exports = sequelize_endpoint(Client, {
-	order: function() {
-		return "lastname ASC";
+	order: function(req) {
+		if (req.query.lat && req.query.lon && req.query.order === "distance") {
+			// Evitar sql injection usando parseFloat
+			var lat = parseFloat(req.query.lat);
+			var lon = parseFloat(req.query.lon);
+
+			console.log("order by distance from (" + lat + " " + lon + ")");
+
+			return "ST_Distance(location, ST_GeographyFromText('SRID=4326;POINT(" + lat + " " + lon + ")'))";
+		} else {
+			return "lastname ASC";
+		}
 	}
 });
