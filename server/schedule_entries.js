f6075181afbdfc5fbae94922e17b0dbd184401de
Dario Seminara <darios3@gmail.com>
2016-05-01 23:30:13 -0300

    Implemented filters on schedule_entries: client_id, seller_id and day_of_week

--- server/schedule_entries.js
+++ server/schedule_entries.js
@@ -1,5 +1,17 @@
 var sequelize_endpoint = require("./sequelize_endpoint");
 var ScheduleEntry = require("./models/schedule_entry");
 
-module.exports = sequelize_endpoint(ScheduleEntry);
+var filterFields = ['day_of_week', 'client_id', 'seller_id'];
+module.exports = sequelize_endpoint(ScheduleEntry, {
+	where: function(req) {
+		var filters = {};
+		filterFields.forEach(function(field) {
+			if (req.query[field]) {
+				filters[field] = req.query[field];
+			}
+		});
+
+		return filters;
+	}
+});
 
