b163cd57b1f34f55489ddbdb889767a371d5c378
Dario Seminara <dseminara@tbx.net.com>
2016-05-09 16:52:22 -0300

    implemented placeholder for push notifications

--- server/models/schedule_entry.js
+++ server/models/schedule_entry.js
@@ -1,8 +1,20 @@
 var Sequelize = require("sequelize");
 var sequelize = require("../domain/sequelize");
+var push_notification_config = require("../../config/push_notification.json");
 
 var Client = require("./client");
 var Seller = require("./seller");
+
+var afterCreate = function(instance) {
+	return Client.findOne({where: {id: instance.get('client_id')}})
+		.then(function(client) {
+			// TODO: enviar push notification
+			console.log("Agregado schedule entry para client");
+			console.log(client.dataValues);
+
+			console.log("TODO enviar push notification con key: " + push_notification_config.key);
+		});
+};
 
 var ScheduleEntry = sequelize.define('schedule_entries', {
   day_of_week: Sequelize.INTEGER,
@@ -12,6 +24,9 @@ var ScheduleEntry = sequelize.define('schedule_entries', {
   freezeTableName: true,
   updatedAt: 'last_modified',
   createdAt: 'date_created',
+  hooks: {
+	afterCreate: afterCreate
+  }
 });
 
 ScheduleEntry.belongsTo(Client, {foreignKey: 'client_id'});
