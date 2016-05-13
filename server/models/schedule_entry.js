3ceb1f430f64d1df4d2be92d32b2f46806dcbb7e
Dario Seminara <darios3@gmail.com>
2016-05-13 00:19:08 -0300

    Implemented push for new clients assigned on schedule

--- server/models/schedule_entry.js
+++ server/models/schedule_entry.js
@@ -3,6 +3,25 @@ var sequelize = require("../domain/sequelize");
 
 var Client = require("./client");
 var Seller = require("./seller");
+var Device = require("./device");
+
+var _ = require("underscore");
+var q = require("q");
+var push = q.denodeify(require("../domain/push").pushNewClientNotification);
+
+var afterCreate = function(instance, options) {
+	var seller_id = instance.get("seller_id");
+	var client_id = instance.get("client_id");
+    return q.all([
+    	Device.findAll({where: {seller_id: seller_id}, group: ['registration_id'], attributes: ['registration_id']}),
+    	Client.findOne({where: {id: client_id}})
+    ])
+    	.spread(function(devices, client) {
+    		var tokens = devices.map(_.property('dataValues')).map(_.property("registration_id"));
+    		return push(client.get("id"), client.get("name"), client.get("thumbnail"), tokens);
+    	})
+    	.catch(console.error.bind(console));
+};
 
 var ScheduleEntry = sequelize.define('schedule_entries', {
   day_of_week: Sequelize.INTEGER,
@@ -11,7 +30,10 @@ var ScheduleEntry = sequelize.define('schedule_entries', {
 }, {
   freezeTableName: true,
   updatedAt: 'last_modified',
-  createdAt: 'date_created'
+  createdAt: 'date_created',
+  hooks: {
+  	afterCreate: afterCreate
+  }
 });
 
 ScheduleEntry.belongsTo(Client, {foreignKey: 'client_id'});
