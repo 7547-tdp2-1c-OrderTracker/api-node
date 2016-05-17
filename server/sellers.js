4f98ab054bfc8834eaa5dcc179e9e43ac0c6a881
Dario Seminara <darios3@gmail.com>
2016-05-17 00:31:40 -0300

    fixed reports api

--- server/sellers.js
+++ server/sellers.js
@@ -35,39 +35,59 @@ app.put("/:seller_id/devices/register", promised(function(req) {
 }));
 
 app.get("/:seller_id/reports", promised(function(req) {
-	var now = req.query.date ? moment(req.query.date) : moment();
-	var day_of_week = moment(now).day();
+	var now;
+	var day_of_week;
 
 	var start, end;
-
-	if (req.query.start_date) {
-		start = moment(req.query.start_date);
+	if (req.query.start_date || req.query.end_date) {
+		now = moment(req.query.start_date);
+		if (req.query.start_date) {
+			start = moment(req.query.start_date);
+		}
+		if (req.query.end_date) {
+			end = moment(req.query.end_date);
+		}
 	} else {
-		var start = now.clone();
+		now = moment();
+		start = now.clone();
 		start.set("hours",0)
 		start.set("minutes",0)
 		start.set("seconds",0)
 	}
 
-	if (req.query.end_date) {
-		end = moment(req.query.end_date);
+	day_of_week = moment(now).day();
+	var date_condition, updated_at_condition, extra_params;
+
+	// siempre va a haber o start, o end, o ambos (si no llega ni start_date ni end_date, toma la fecha de hoy)
+	if (end) {
+		if (start) {
+			date_condition = "v.date >= ? AND v.date <= ?";
+			updated_at_condition = "o.updated_at >= ? AND o.updated_at <= ?";
+			extra_params = [start.toISOString(), end.toISOString()];
+		} else {
+			date_condition = "v.date <= ?";
+			updated_at_condition = "o.updated_at <= ?";
+			extra_params = [end.toISOString()];
+		}
 	} else {
-		end = start.clone().add(1, 'day');
+		date_condition = "v.date >= ?";
+		updated_at_condition = "o.updated_at >= ?";
+		extra_params = [start.toISOString()];
 	}
 
 	var seller_id = req.params.seller_id;
 	return q.all([
-		sequelize.query("SELECT COUNT(*) as count FROM (SELECT client_id FROM visits as v JOIN schedule_entries as s ON v.schedule_entry_id = s.id WHERE s.seller_id = ? AND v.date >= ? AND v.date <= ? AND day_of_week = ? GROUP BY client_id) as clients", {
-			replacements: [seller_id, start.toISOString(), end.toISOString(), day_of_week]
+		sequelize.query("SELECT COUNT(*) as count FROM (SELECT client_id FROM visits as v JOIN schedule_entries as s ON v.schedule_entry_id = s.id WHERE s.seller_id = ? AND day_of_week = ? AND " + date_condition + " GROUP BY client_id) as clients", {
+			replacements: [seller_id, day_of_week].concat(extra_params)
 		}),
-		sequelize.query("SELECT COUNT(*) as count FROM (SELECT client_id FROM visits as v JOIN schedule_entries as s ON v.schedule_entry_id = s.id WHERE s.seller_id = ? AND v.date >= ? AND v.date <= ? AND day_of_week != ? GROUP BY client_id) as clients", {
-			replacements: [seller_id, start.toISOString(), end.toISOString(), day_of_week]
+		sequelize.query("SELECT COUNT(*) as count FROM (SELECT client_id FROM visits as v JOIN schedule_entries as s ON v.schedule_entry_id = s.id WHERE s.seller_id = ? AND day_of_week != ? AND " + date_condition + " GROUP BY client_id) as clients", {
+			replacements: [seller_id, day_of_week].concat(extra_params)
 		}),
-		sequelize.query("SELECT SUM(total_price) as total, o.currency as currency FROM orders as o WHERE o.status = 'confirmed' AND o.seller_id = ? AND o.updated_at >= ? AND o.updated_at <= ? GROUP BY currency", {
-			replacements: [seller_id, start.toISOString(), end.toISOString()]
+		sequelize.query("SELECT SUM(total_price) as total, o.currency as currency FROM orders as o WHERE o.status = 'confirmed' AND o.seller_id = ? AND " + updated_at_condition + " GROUP BY currency", {
+			replacements: [seller_id].concat(extra_params)
 		}),
-		sequelize.query("SELECT COUNT(*) as count FROM orders as o WHERE o.status = 'confirmed' AND o.seller_id = ? AND o.updated_at >= ? AND o.updated_at <= ?", {
-			replacements: [seller_id, start.toISOString(), end.toISOString()]
+		sequelize.query("SELECT COUNT(*) as count FROM orders as o WHERE o.status = 'confirmed' AND o.seller_id = ? AND " + updated_at_condition, {
+			replacements: [seller_id].concat(extra_params)
 		})
 
 	]).spread(function(visits, out_of_route_visits, total_price, orders) {
