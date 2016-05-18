6037e869ef6910d1c9f37e871093f444ac6c4b68
Dario Seminara <darios3@gmail.com>
2016-05-17 23:25:41 -0300

    Implemented product_id and brand_id promotion validations

--- server/models/promotion.js
+++ server/models/promotion.js
@@ -12,9 +12,29 @@ var afterCreate = function(instance, options) {
 	return push(instance.get("id"), instance.get("name")).catch(console.error.bind(console));
 };
 
+var beforeCreate = function(instance, options) {
+	if (instance.product_id && instance.brand_id) {
+		throw {error: {key: 'BAD_PROMOTION', value: "una promocion no puede se de un producto y una marca a la vez"}, status: 400};
+	}
+};
+
+var beforeUpdate = function(instance, options) {
+	if (options.fields.indexOf("product_id") !== -1 && options.fields.indexOf("brand_id") !== -1) {
+		throw {error: {key: 'BAD_PROMOTION', value: "una promocion no puede se de un producto y una marca a la vez"}, status: 400};
+	}
+
+	if (options.fields.indexOf("product_id") !== -1) {
+		instance.brand_id = null;
+	}
+
+	if (options.fields.indexOf("brand_id") !== -1) {
+		instance.product_id = null;
+	}
+};
+
 var Promotion = sequelize.define('promotions', {
   name: Sequelize.STRING,
-  min_quantity: {type: Sequelize.INTEGER, defaultValue: 0},
+  min_quantity: {type: Sequelize.INTEGER, defaultValue: 0},	
   percent: Sequelize.INTEGER,
   begin_date: Sequelize.DATE,
   end_date: Sequelize.DATE,
@@ -25,6 +45,8 @@ var Promotion = sequelize.define('promotions', {
   updatedAt: 'last_modified',
   createdAt: 'date_created',
   hooks: {
+  	beforeCreate: beforeCreate,
+  	beforeUpdate: beforeUpdate,
   	afterCreate: afterCreate
   }
 });
