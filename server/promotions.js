8d7b1901e2265d019c78fed2e5c4068f9d6ecd5a
Dario Seminara <darios3@gmail.com>
2016-05-21 13:11:38 -0300

    include Brand and Product data on promotions

--- server/promotions.js
+++ server/promotions.js
@@ -1,4 +1,7 @@
 var sequelize_endpoint = require("./sequelize_endpoint");
 var Promotion = require("./models/promotion");
 
-module.exports = sequelize_endpoint(Promotion);
+var Product = require("./models/product");
+var Brand = require("./models/brand");
+
+module.exports = sequelize_endpoint(Promotion, {include: [{model: Product}, {model: Brand}]});
