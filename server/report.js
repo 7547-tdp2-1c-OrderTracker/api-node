553bb7558662058078d2c49d98bbd0b9d3d56028
Dario Seminara <darios3@gmail.com>
2016-05-22 12:51:58 -0300

    implemented report brandSales

--- server/report.js
+++ server/report.js
@@ -26,6 +26,43 @@ var promised = function(f) {
 	};
 };
 
+var brandsSalesQuery = [
+"select b.id, b.name, b.picture, b.code, sum(o.unit_price * o.quantity) as total_amount, count(*) as items_amount",
+"from order_entries as o join products as p on o.product_id = p.id join brands as b on p.brand_id = b.id",
+"where o.currency = ? and o.updated_at > ? and o.updated_at < ?",
+"group by b.id"
+].join(" ");
+
+app.get("/brandsSales", promised(function(req) {
+	var processBrand = function(brand) {
+		return {
+			brand: {
+				id: brand.id,
+				name: brand.name,
+				code: brand.code,
+				picture: brand.picture
+			},
+			total_amount: parseInt(brand.total_amount),
+			items_amount: parseInt(brand.items_amount)
+		};
+	};
+
+	var date = req.query.date || moment().format("MM-YYYY");
+	var beginDate = moment(date, "MM-YYYY");
+	var endDate = beginDate.clone().add(1, 'month');
+
+	return sequelize.query(brandsSalesQuery, {replacements: [req.query.currency || "ARS", beginDate.toISOString(), endDate.toISOString()]})
+		.then(function(results) {
+			return {
+				body: {
+					date: date,
+					report: results[0].map(processBrand)
+				},
+				status: 200
+			};
+		});
+}));
+
 var totalsQuery = "select s.id as id, s.name as name, s.lastname as lastname, SUM(o.total_price) as total from sellers as s join orders as o on o.seller_id = s.id where currency = ? and o.updated_at > ? and o.updated_at < ? group by s.id"
 
 app.get("/sellers/top10", promised(function(req) {
